547f2199e7e3e068d0dae61fcdbf59e0016d0233
EkaterinaKarelina <katekrln0309@gmail.com>
2023-10-24 21:45:28 +0300

    Создали структуру таблици

---
+++ todo/mainpage/models.py
@@ -0,0 +1,15 @@
+
+from django.db import models
+from datetime import datetime
+
+# В этом файле принято создавать классы, описывающие
+# структуры необходимых приложению таблиц
+
+
+# https://docs.djangoproject.com/en/4.2/topics/db/models/
+class Task(models.Model):
+     # id создастся сам!
+     start = models.DateTimeField(default=datetime.now)
+     end = models.DateTimeField()
+     description = models.CharField(max_length=256, default='')
+     done = models.BooleanField(default=False)
