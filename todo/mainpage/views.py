d8ce4509998c258699582309f1e1cc1e36e347de
EkaterinaKarelina <katekrln0309@gmail.com>
2023-10-24 22:53:31 +0300

    Передача данных из базы на сайт

--- todo/mainpage/views.py
+++ todo/mainpage/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
+from .models import Task
+
 
 def index(request):
+    result = ""
+    for t in Task.objects.all():
+        result += t.description
+    kateg = ""    
+    for a in Kategorii.objects.all():
+        kateg += a.kategoriya    
     return render(
         request,
         "mainpage/index.html",
-        {}
+        {"Задачи": result, "Категории": kateg}
+   
     )
+
+
+
+from .models import Kategorii
+
+
+#def index(request):
+
+ #   return render(
+ #       request,
+ #       "mainpage/index.html",
+ #       {"zadachi":Kategorii.objects.all()}
+   # )
