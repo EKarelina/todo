52eef2a285ed0143686ca44fa77582c530628005
EkaterinaKarelina <katekrln0309@gmail.com>
2023-10-25 20:02:37 +0300

    Вывели список дел из базы данных в шаблон с помощью цикла

--- todo/mainpage/views.py
+++ todo/mainpage/views.py
@@ -4,15 +4,15 @@ from .models import Task
 
 def index(request):
     result = ""
-    for t in Task.objects.all():
-        result += t.description
+   # for t in Task.objects.all():
+    #    result += t.description
     kateg = ""    
     for a in Kategorii.objects.all():
         kateg += a.kategoriya    
     return render(
         request,
         "mainpage/index.html",
-        {"Задачи": result, "Категории": kateg}
+        {"Задачи": Task.objects.all(), "Категории": kateg} # Kонтекст передаваемых переменных
    
     )
 
